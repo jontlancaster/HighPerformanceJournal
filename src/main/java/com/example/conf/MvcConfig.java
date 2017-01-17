7d52b18dbb3d686ba86dc70701fd1104b71a974f
fernando salazar <fersal69@gmail.com>
2017-01-16 17:07:22 -0700

    Added Spring-Security, basic HTML templates, thymeleaf for UI, URL mappings

---
+++ src/main/java/com/example/conf/MvcConfig.java
@@ -0,0 +1,21 @@
+package com.example.conf;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by fernandosalazar on 1/16/17.
+ */
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter{
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/admin").setViewName("admin");
+        registry.addViewController("/journal").setViewName("journal");
+        registry.addViewController("/error").setViewName("error");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
