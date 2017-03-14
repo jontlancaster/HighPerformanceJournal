7a40bd90b0a2fd86c0c3ac173c87e0073674fe3e
jontlancaster <jontlancaster@gmail.com>
2017-03-14 10:15:12 -0600

    Added user controller
    added user controller and moved existing applicable @RestControllers to
    that class.

--- src/main/java/com/journal/ClearlinkJournal.java
+++ src/main/java/com/journal/ClearlinkJournal.java
@@ -21,20 +21,11 @@ import java.util.List;
 public class ClearlinkJournal {
 
 	@Autowired
-	private UserRepository userRepository;
-	@Autowired
 	private UserTypeRepository userTypeRepository;
 	@Autowired
 	private JournalRepository journalRepository;
 	@Autowired
 	private JournalEntryRepository journalEntryRepository;
-	@Autowired
-	private UserManagerService userManager;
-
-	@RequestMapping("/users/{username}")
-	public User user(@PathVariable("username") String username) {
-		return userRepository.findByUsername(username);
-	}
 
 	@RequestMapping("/userTypes/{userType}")
 	public UserType userType(@PathVariable("userType") String userType) { return userTypeRepository.findByUserType(userType); }
@@ -47,34 +38,6 @@ public class ClearlinkJournal {
 
 	@RequestMapping("/journalEntries/{entries}")
 	public List<JournalEntry> getJournalEntries(@PathVariable("entries") int journalId) { return journalEntryRepository.findByJournalJournalId(journalId); }
-
-	@RequestMapping("users/find")
-	public User findUser(@RequestParam("username") String username) { return userManager.findUser(username); }
-
-	@RequestMapping("users/disable")
-	public boolean disableUser(@RequestParam("username") String username) {
-		User user = userRepository.findByUsername(username);
-		return userManager.disableUser(user);
-	}
-
-	@RequestMapping("users/enable")
-	public boolean enableUser(@RequestParam("username") String username) {
-		User user = userRepository.findByUsername(username);
-		return userManager.enableUser(user);
-	}
-
-	@RequestMapping(value="users/create")
-	public boolean createUser(@RequestParam("firstName") String firstName, @RequestParam("lastName") String lastName,
-							  @RequestParam("username") String username, @RequestParam("password") String password,
-							  @RequestParam("userType") int userType) {
-		User user = new User();
-		user.setFirstName(firstName);
-		user.setLastName(lastName);
-		user.setUsername(username);
-		user.setPassword(password);
-		user.setUserType(userType);
-		return userManager.createNewUser(user);
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClearlinkJournal.class, args);
---
+++ src/main/java/com/journal/controllers/UserController.java
@@ -0,0 +1,54 @@
+package com.journal.controllers;
+
+import com.journal.entities.User;
+import com.journal.repositories.UserRepository;
+import com.journal.services.UserManagerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by JLancaster on 3/13/2017.
+ */
+@RestController
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private UserManagerService userManager;
+
+    @RequestMapping(value="users/create")
+    public boolean createUser(@RequestParam("firstName") String firstName, @RequestParam("lastName") String lastName,
+                              @RequestParam("username") String username, @RequestParam("password") String password,
+                              @RequestParam("userType") int userType) {
+        User user = new User();
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setUserType(userType);
+        return userManager.createNewUser(user);
+    }
+
+    @RequestMapping("users/find")
+    public User findUser(@RequestParam("username") String username) { return userManager.findUser(username); }
+
+    @RequestMapping("/users/{username}")
+    public User user(@PathVariable("username") String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    @RequestMapping("users/disable")
+    public boolean disableUser(@RequestParam("username") String username) {
+        User user = userRepository.findByUsername(username);
+        return userManager.disableUser(user);
+    }
+
+    @RequestMapping("users/enable")
+    public boolean enableUser(@RequestParam("username") String username) {
+        User user = userRepository.findByUsername(username);
+        return userManager.enableUser(user);
+    }
+}
