563808856d8b89b53cf2a40c5e27532c322a14ef
jontlancaster <jontlancaster@gmail.com>
2017-07-25 22:30:40 -0600

    Updated the mysql files
    Updated the table creation and fill db with test data.

--- mysql queries/Create Tables.sql
+++ mysql queries/Create Tables.sql
@@ -4,19 +4,19 @@ USE clearlinkjournal;
 
 CREATE table users
 (
-user_id LONG PRIMARY KEY AUTO_INCREMENT,
+user_id bigint PRIMARY KEY AUTO_INCREMENT,
 first_name varchar(255) NOT NULL,
 last_name varchar(255),
 username varchar(60) NOT NULL UNIQUE,
 password varchar(60) NOT NULL,
 enabled BOOL NOT NULL DEFAULT true,
-created_date DATE NOT NULL DEFAULT current_timestamp,
+created_date DATE NOT NULL,
 modified_date DATETIME NOT NULL DEFAULT current_timestamp ON UPDATE current_timestamp
 );
 
 CREATE Table user_roles
 (
-  user_role_id LONG PRIMARY KEY AUTO_INCREMENT,
+  user_role_id bigint PRIMARY KEY AUTO_INCREMENT,
   username varchar(60) NOT NULL,
   role VARCHAR(60) NOT NULL,
   UNIQUE KEY uni_username_role (role, username),
@@ -27,18 +27,18 @@ CREATE Table user_roles
 
 CREATE table journals
 (
-journal_id LONG PRIMARY KEY AUTO_INCREMENT,
+journal_id bigint PRIMARY KEY AUTO_INCREMENT,
 journal_name varchar(255) NOT NULL,
-user_id LONG NOT NULL,
-created_date DATE NOT NULL DEFAULT current_timestamp,
+user_id bigint NOT NULL,
+created_date DATE NOT NULL,
 modified_date DATETIME NOT NULL DEFAULT current_timestamp ON UPDATE current_timestamp,
 FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE
 );
 
 CREATE Table journal_entries
 (
-journal_entry_id LONG PRIMARY KEY AUTO_INCREMENT,
-journal_id LONG NOT NULL,
+journal_entry_id bigint PRIMARY KEY AUTO_INCREMENT,
+journal_id bigint NOT NULL,
 positive_review varchar(1000),
 goal varchar(1000),
 momentum varchar(1000),
@@ -48,7 +48,7 @@ determination int NOT NULL,
 motivation int NOT NULL,
 attitude int NOT NULL,
 personal_impact int NOT NULL,
-created_date DATE NOT NULL DEFAULT current_timestamp,
+created_date DATE NOT NULL,
 modified_date DATETIME NOT NULL DEFAULT current_timestamp ON UPDATE current_timestamp,
 FOREIGN KEY (journal_id) REFERENCES journals (journal_id) ON DELETE CASCADE
 );
--- mysql queries/fill db.sql
+++ mysql queries/fill db.sql
@@ -1,14 +1,14 @@
 USE clearlinkjournal;
 
-insert into users (first_name, last_name, username, password)
-values ('Jon', 'Lancaster', 'jonlan', 'pass'),
-  ('Fernando', 'Salazar', 'fersal', 'ferSal'),
-  ('Scuba', 'Steve', 'steve', 'passwd'),
-  ('Fred', 'Rogers', 'mrrogers', 'neighborhood'),
-  ('Big', 'Bird', 'bigbird', 'yellow'),
-  ('Oscar', 'Grouch', 'grouchy', 'garbage'),
-  ('The', 'Count', 'thecount', 'ILoveToCount'),
-  ('Cookie', 'Monster', 'cookies', 'cookies');
+insert into users (first_name, last_name, username, password, created_date)
+values ('Jon', 'Lancaster', 'jonlan', 'pass', curdate()),
+  ('Fernando', 'Salazar', 'fersal', 'ferSal', curdate()),
+  ('Scuba', 'Steve', 'steve', 'passwd', curdate()),
+  ('Fred', 'Rogers', 'mrrogers', 'neighborhood', curdate()),
+  ('Big', 'Bird', 'bigbird', 'yellow', curdate()),
+  ('Oscar', 'Grouch', 'grouchy', 'garbage', curdate()),
+  ('The', 'Count', 'thecount', 'ILoveToCount', curdate()),
+  ('Cookie', 'Monster', 'cookies', 'cookies', curdate());
 
 insert into user_roles (username, role)
 values ('jonlan', 'ROLE_ADMIN'),
@@ -26,34 +26,34 @@ values ('jonlan', 'ROLE_ADMIN'),
   ('thecount', 'ROLE_USER'),
   ('cookies', 'ROLE_USER');
 
-insert into journals (journal_name, user_id)
-values ('Jons Journal', 1),
-  ('fernandos journal', 2),
-  ('scubas journal', 3),
-  ('freds journal', 4),
-  ('bigs journal', 5),
-  ('oscars journal', 6),
-  ('thes journal', 7),
-  ('cookies journal', 8);
+insert into journals (journal_name, user_id, created_date)
+values ('Jons Journal', 1, curdate()),
+  ('fernandos journal', 2, curdate()),
+  ('scubas journal', 3, curdate()),
+  ('freds journal', 4, curdate()),
+  ('bigs journal', 5, curdate()),
+  ('oscars journal', 6, curdate()),
+  ('thes journal', 7, curdate()),
+  ('cookies journal', 8, curdate());
 
-insert into journal_entries (journal_id, positive_review, goal, momentum, mental_toughness, willingness, determination, motivation, attitude, personal_impact)
-values (1, 'positive review', 'goal', 'momentum', 1, 1, 1, 1, 1, 1),
-  (1, 'positive review', 'goal', 'momentum', 1, 1, 1, 1, 1, 1),
-  (2, 'positive review', 'goal', 'momentum', 1, 1, 1, 1, 1, 1),
-  (2, 'positive review', 'goal', 'momentum', 1, 1, 1, 1, 1, 1),
-  (3, 'positive review', 'goal', 'momentum', 1, 1, 1, 1, 1, 1),
-  (3, 'positive review', 'goal', 'momentum', 1, 1, 1, 1, 1, 1),
-  (4, 'positive review', 'goal', 'momentum', 1, 1, 1, 1, 1, 1),
-  (4, 'positive review', 'goal', 'momentum', 1, 1, 1, 1, 1, 1),
-  (5, 'positive review', 'goal', 'momentum', 1, 1, 1, 1, 1, 1),
-  (5, 'positive review', 'goal', 'momentum', 1, 1, 1, 1, 1, 1),
-  (6, 'positive review', 'goal', 'momentum', 1, 1, 1, 1, 1, 1),
-  (6, 'positive review', 'goal', 'momentum', 1, 1, 1, 1, 1, 1),
-  (7, 'positive review', 'goal', 'momentum', 1, 1, 1, 1, 1, 1),
-  (7, 'positive review', 'goal', 'momentum', 1, 1, 1, 1, 1, 1),
-  (8, 'positive review', 'goal', 'momentum', 1, 1, 1, 1, 1, 1),
-  (8, 'positive review', 'goal', 'momentum', 1, 1, 1, 1, 1, 1),
-  (8, 'positive review', 'goal', 'momentum', 1, 1, 1, 1, 1, 1),
-  (8, 'positive review', 'goal', 'momentum', 1, 1, 1, 1, 1, 1);
+insert into journal_entries (journal_id, positive_review, goal, momentum, mental_toughness, willingness, determination, motivation, attitude, personal_impact, created_date)
+values (1, 'positive review', 'goal', 'momentum', 1, 1, 1, 1, 1, 1, curdate()),
+  (1, 'positive review', 'goal', 'momentum', 1, 1, 1, 1, 1, 1, curdate()),
+  (2, 'positive review', 'goal', 'momentum', 1, 1, 1, 1, 1, 1, curdate()),
+  (2, 'positive review', 'goal', 'momentum', 1, 1, 1, 1, 1, 1, curdate()),
+  (3, 'positive review', 'goal', 'momentum', 1, 1, 1, 1, 1, 1, curdate()),
+  (3, 'positive review', 'goal', 'momentum', 1, 1, 1, 1, 1, 1, curdate()),
+  (4, 'positive review', 'goal', 'momentum', 1, 1, 1, 1, 1, 1, curdate()),
+  (4, 'positive review', 'goal', 'momentum', 1, 1, 1, 1, 1, 1, curdate()),
+  (5, 'positive review', 'goal', 'momentum', 1, 1, 1, 1, 1, 1, curdate()),
+  (5, 'positive review', 'goal', 'momentum', 1, 1, 1, 1, 1, 1, curdate()),
+  (6, 'positive review', 'goal', 'momentum', 1, 1, 1, 1, 1, 1, curdate()),
+  (6, 'positive review', 'goal', 'momentum', 1, 1, 1, 1, 1, 1, curdate()),
+  (7, 'positive review', 'goal', 'momentum', 1, 1, 1, 1, 1, 1, curdate()),
+  (7, 'positive review', 'goal', 'momentum', 1, 1, 1, 1, 1, 1, curdate()),
+  (8, 'positive review', 'goal', 'momentum', 1, 1, 1, 1, 1, 1, curdate()),
+  (8, 'positive review', 'goal', 'momentum', 1, 1, 1, 1, 1, 1, curdate()),
+  (8, 'positive review', 'goal', 'momentum', 1, 1, 1, 1, 1, 1, curdate()),
+  (8, 'positive review', 'goal', 'momentum', 1, 1, 1, 1, 1, 1, curdate());
 
 
